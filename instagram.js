407364c12470ae551a134bef9e28bcb19dab1bcb
Ganhatos <ganhatos@gmail.com>
2021-04-12 13:14:05 -0300

    adicionado findbypk, findone, create, update e destroy

--- instagram.js
+++ instagram.js
@@ -1,6 +1,9 @@
-const {Usuario, Post, Comentario} = require('./models');
+const {Usuario, Post, Comentario, sequelize} = require('./models');
+const {Op} = require('sequelize');
 
-Usuario.findAll()
+// Find all
+
+/*Usuario.findAll()
 .then((resultado) => {
     console.table(resultado.map(user => user.toJSON()));
 });
@@ -14,3 +17,145 @@ Comentario.findAll()
 .then((resultado) => {
     console.table(resultado.map(comentario => comentario.toJSON()));
 });
+
+// Find By PK
+
+Usuario.findByPk(1)
+.then((resultado) => {
+    console.table(resultado.toJSON());
+});
+
+Post.findByPk(1)
+.then((resultado) => {
+    console.table(resultado.toJSON());
+});
+
+Comentario.findByPk(1)
+.then((resultado) => {
+    console.table(resultado.toJSON());
+});
+
+// Find One
+
+Usuario.findOne({
+    where: {
+        nome: {[Op.like]: 'Gabriel%'}
+    }
+})
+.then((resultado) => {
+    console.table(resultado.toJSON());
+});
+
+Post.findOne({
+    where: {
+        texto: {[Op.like]: 'Oi%'}
+    }
+})
+.then((resultado) => {
+    console.table(resultado.toJSON());
+});
+
+Comentario.findOne({
+    where: {
+        texto: {[Op.like]: '%Fim%'}
+    }
+})
+.then((resultado) => {
+    console.table(resultado.toJSON());
+});
+
+Usuario.findAll({
+    order: [
+        ['nome', 'ASC']
+    ],
+    limit: 2,
+    offset: 2
+})
+.then((resultado) => {
+    console.table(resultado.map(user => user.toJSON()));
+});
+
+Usuario.findAll({
+    where: {
+        nome:{[Op.like]: '%a%'}
+    }
+})
+.then((resultado) => {
+    console.table(resultado.map(user => user.toJSON()));
+});
+
+Usuario.findAll({
+    where: {
+        nome:{[Op.notLike]: '%a%'}
+    }
+})
+.then((resultado) => {
+    console.table(resultado.map(user => user.toJSON()));
+});
+
+for (let i = 0; i < 6; i+= 2) {
+    Comentario.findAll({
+        limit: 2,
+        offset: i        
+    })
+    .then((resultado) => {
+        console.table(resultado.map(comentario => comentario.toJSON()));
+    });
+}
+
+Usuario.create({
+    nome: 'Iago',
+    email: 'iago@digitalhouse.com',
+    senha: 'dev123'
+}).then((resultado) => {
+    console.log(resultado.toJSON());
+});
+
+Usuario.update({
+    senha: 'novasenha123'
+},
+    {
+    where: {
+        id: 5
+    }
+}).then((resultado) => {
+    console.log(resultado);
+});
+
+Usuario.destroy({
+    where: {
+        id: 6
+    }
+}).then((resultado) => {
+    console.log(resultado);
+});
+
+Post.create({
+    texto: 'Aprendendo sequelize!',
+    img: null,
+    usuarios_id: 13,
+    n_likes: 0
+}
+).then((resultado) => {
+    console.log(resultado.toJSON());
+});
+
+Usuario.update({
+    senha: '123'
+},
+{
+    where: {
+        nome: {[Op.like]: 'Sergio%'}
+    }
+
+}).then((resultado) => {
+    console.table(resultado);
+});
+
+Usuario.destroy({
+    where: {
+        id: 3
+    }
+}).then((resultado) => {
+    console.log(resultado);
+});*/
